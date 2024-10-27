9f533557683bdb4023d0a62b52518d67a38d3771
Ilya <i.a.4esnokov@gmail.com>
2024-10-27 18:51:35 +0300

    first page

---
+++ mind-match-project/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Main from "./components/entry/Main/Main";
+
+function App() {
+    return (
+        <div>
+            <Main />
+        </div>
+    );
+}
+
+export default App;
---
+++ mind-match-project/src/components/entry/Main/Main.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./Main.module.css"
+import { NavLink } from "react-router-dom";
+import google from "../../../assets/images/google.svg"
+import yandex from "../../../assets/images/yandex.svg"
+import apple from "../../../assets/images/apple.svg"
+
+function Main() {
+    return (
+        <div className={styles.mainBlock}>
+            <div className={styles.centralBlock}>
+                <h1 className={styles.mainText}>Welcome <span className={styles.mainText_subBlock}>to <span className={styles.subMainText}>MindMatch!</span></span></h1>
+            </div>
+            <div className={styles.regBlock}>
+                <p className={styles.mediumText}>Sign up to continue:</p>
+                <NavLink to="/registration" className={styles.button}>Continue with email</NavLink>
+                <NavLink to="/login" className={styles.smallText}>Login to account</NavLink>
+            </div>
+            <div className={styles.nextRegBlock}>
+                <p className={styles.mediumText}>Or sign up with:</p>
+                <div className={styles.logos}>
+                    <img src={google} className={styles.logo} alt="google" />
+                    <img src={yandex} className={styles.logo} alt="yandex" />
+                    <img src={apple} className={styles.logo} alt="apple" />
+                </div>
+            </div>
+            <div className={styles.footerBlock}>
+                <p className={styles.footerBlock_text}>Terms of use</p>
+                <p className={styles.footerBlock_text}>Privacy Policy</p>
+            </div>
+        </div>
+    );
+}
+
+export default Main;
