1749f990c82e2a2bb6b4fc7f3b312c9c89345c4e
Ilya <i.a.4esnokov@gmail.com>
2024-11-27 22:18:49 +0300

    tell-about-yourself in process

--- mind-match-project/src/components/entry/registration/TellAboutYourself/TellAboutYourself.jsx
+++ mind-match-project/src/components/entry/registration/TellAboutYourself/TellAboutYourself.jsx
@@ -45,7 +45,7 @@ export default function TellAboutYourself() {
                 >
                     <p className={styles.text}>Industry:</p>
                     <input
-                        list="industries"
+                        // list="industries"
                         className={styles.input}
                         {...register("activity", {
                             required: "Industry is required.",
@@ -57,17 +57,55 @@ export default function TellAboutYourself() {
                         type="text"
                         placeholder="Choose your Industry..."
                     />
-                    <datalist id="industries" style={{backgroundColor: "black"}}>
+                    <p className={styles.error_text}>
+                        {errors.activity ? (
+                            <span>{errors.activity.message}</span>
+                        ) : (
+                            ""
+                        )}
+                    </p>
+                    {/* <datalist id="industries" style={{backgroundColor: "black"}}>
                         <option value="Developer"></option>
                         <option value="Designer"></option>
                         <option value="Analyst"></option>
-                    </datalist>
+                    </datalist> */}
+                </div>
+                <div className={styles.wrap_input}>
+                    <p className={styles.text}>Description:</p>
+                    <textarea
+                        {...register("info", {
+                            maxLength: {
+                                value: 1500,
+                                message: "Your description is too long.",
+                            },
+                        })}
+                        placeholder="Description 1500 characters..."
+                        className={`${styles.input} ${styles.input_textarea}`}
+                    ></textarea>
                 </div>
             </form>
-            <input type="text" />
-            <textarea></textarea>
-            <div>tags</div>
-            <Button type="submit" text="Next" />
+            <div className={styles.tags_container}>
+                <p className={styles.text}>Tags:</p>
+                <hr className={styles.line} />
+                <div className={styles.tags}>
+                    <div className={styles.tag}>
+                        <p className={styles.tag_text}>Sports</p>
+                    </div>
+                    <div className={styles.tag}>
+                        <p className={styles.tag_text}>Initiative</p>
+                    </div>
+                    <div className={styles.tag}>
+                        <p className={styles.tag_text}>Enthusiast</p>
+                    </div>
+                    <div className={styles.tag}>
+                        <p className={styles.tag_text}>Closed</p>
+                    </div>
+                    <div className={styles.tag}>
+                        <p className={styles.tag_text}>Modest</p>
+                    </div>
+                </div>
+            </div>
+            <Button type="submit" text="Next" disabled={!isValid} />
         </div>
     );
 }
